5466b493312643a94153b7b47f2e5decce350115
Brian Parker <btparker70@gmail.com>
2020-11-06 14:22:49 -0800

    cleaned up code and added more comments

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,38 +1,41 @@
-// Saved cities from searches
-var savedCities = JSON.parse(localStorage.getItem("cities")) || [];
-
+// Assets
 var cityName = '';
 var lat = 0;
 var lon = 0;
 
-// Clear All button
-$('#clearBtn').on('click', function() {
-  $('#searchList').empty();
-  localStorage.clear();
-  location.reload();
-  $('#clearBtn').css('display', 'none')
-})
+// Search results local storage
+var savedCities = JSON.parse(localStorage.getItem("cities")) || [];
 
-// When list items are clicked
+// SEARCH AREA //
+// Search result list on-clicks
 $('#searchList').on('click', '.list-group-item', function () {
   clearListActive();
   $(this).toggleClass('active');
   dataRender($(this).text());
 })
 
-// Color item
+// Colorize search result list item
 function colorListActive() {
   clearListActive();
   $('#' + ($('#searchInput').val())).toggleClass('active');
 }
 
-// Clear list active color
+// Decolorize search result list item
 function clearListActive() {
   $('.list-group-item').each(function () {
     $(this).removeClass('active');
   })
 }
 
+// Clear all button
+$('#clearBtn').on('click', function () {
+  $('#searchList').empty();
+  localStorage.clear();
+  location.reload();
+  $('#clearBtn').css('display', 'none')
+})
+
+// SEARCH LIST AND LOCAL STORAGE //
 // When search button clicked
 $('#searchBtn').click(function () {
   // Clear list color first
@@ -41,46 +44,45 @@ $('#searchBtn').click(function () {
   // Check if there's duplicate searches
   var found = false;
 
-  for (i = 0; i < savedCities.length; i++) {              // For all saved cities
-    if (savedCities[i].city == $('#searchInput').val()) { // if one matches the search
+  // If user searches for a previously searched city
+  for (i = 0; i < savedCities.length; i++) {              // For all saved city searches
+    if (savedCities[i].city == $('#searchInput').val()) { // If one matches the current search
       var cityName = $('#searchInput').val();
       dataRender(cityName);                               // Display data for that search
       found = true;
-      $(`#${cityName}`).toggleClass('active');            // Change list item to active
-      $('#searchInput').val('');                          // Empty the search box
-      break;                                              // end the function
+      $(`#${cityName}`).toggleClass('active');            // Colorize existing list item
+      $('#searchInput').val('');                          // Empty the search field
+      break;
     }
   }
 
-  // make new object for city search
+  // If user searches a new city
   if ($('#searchInput').val() !== '' && (found == false)) {
+    // Add new city to local storage
     var cityName = $('#searchInput').val();
-
     var newCity = {
       city: cityName
     }
     savedCities.push(newCity);
-
     $('#searchInput').val('');
-    // Add new city to saved cities array
     localStorage.setItem('cities', JSON.stringify(savedCities));
-
 
     // Create new list item
     var newSearchItem = $('<li/>').attr('class', 'list-group-item active span').attr('id', cityName);
     newSearchItem.text(cityName);
     $('#searchList').prepend(newSearchItem);
+
+    // Display clear all button
     if ($('#clearBtn').css('display') == ('none')) {
       $('#clearBtn').css('display', 'block')
     }
 
+    // Display weather data
     dataRender(cityName);
-
   }
-
 })
 
-// Save searches persist on refresh
+// Save search list persists on page refresh
 function renderSavedCities() {
   for (i = 0; i < savedCities.length; i++) {
     var cityName = savedCities[i].city;
@@ -90,43 +92,51 @@ function renderSavedCities() {
     $('#clearBtn').css('display', 'block')
   }
 }
-renderSavedCities()
+renderSavedCities();
 
-
+// WEATHER API CALLS AND PAGE DISPLAY //
 // Displays weather data on page
 function dataRender(cityName) {
   var apiKey = 'c2a92e28123db3bc43faafb0a9eb3c40';
   var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-
+  
+  // API call for search by city name
   $.ajax({
     url: cityURL,
     method: "GET",
-    // error: function () {
-    //   alert('Not a city...');
-    // }
+    error: function () {
+      alert("Sorry, we couldn't find the city you were looking for. Please check the spelling and try again.");
+    }
   }).then(function (response) {
 
+    // Collect latitute and longitude data for searched city
     var lat = response.coord['lat'];
     var lon = response.coord['lon'];
     var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly&appid=' + apiKey;
-    console.log(response);
 
+    // API call using on lat/lon
     $.ajax({
       url: oneCallUrl,
       method: "GET"
     }).then(function (response) {
-      // Current city data display
+
+      // CURRENT CITY WEATHER DISPLAY //
       $('#currentWeatherBlock').css('display', 'block');
+      
+      // Icon
       var iconCode = response.current.weather[0].icon;
       var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-      var imgDiv = $('<img>').attr('src', iconUrl);
+      var imgDiv = $('<img>').attr('src', iconUrl).attr('alt', 'forecast weather icon');
+
+      // Data
       $('#cityNameDisplay').text('City: ' + cityName).append(imgDiv);
       $('#tempDisplay').text('Temperature: ' + ((Math.floor((response.current.temp) - 273.15) * 1.80 + 32)).toFixed(1) + 'F');
       $('#humidityDisplay').text('Humidity: ' + response.current.humidity + '%');
       $('#windSpeedDisplay').text('Wind Speed: ' + Math.floor(response.current.wind_speed * 1.151) + 'mph');
       $('#uvIndexDisplay').text('UV Index: ');
       $('#currentWeatherDisplay').css('display', 'block');
-      // Mouse over effect for uv index colors
+
+      // Mouseover effect for uv index colors
       var uvHoverText = '';
       var uvText = $('<p>' + response.current.uvi + '</p>').css('display', 'inline');
       if (response.current.uvi < 3) {
@@ -155,53 +165,47 @@ function dataRender(cityName) {
       }, function () {
         $(this).css('cursor', 'auto');
       })
-      // Forecast display
+
+      // FORECAST DISPLAY //
       for (i = 0; i < 5; i++) {
         // Day of the week and the date
-
         var dayOfWeek = moment().day();
         $('#day' + i + '').text((moment().day(dayOfWeek + 1 + i).format('dddd')) + ' ' + (moment().add(i + 1, 'days').format('MM/DD/YYYY')));
         $('#day' + i + '').append('<br>');
+
+        // Icon
         var iconCode = response.daily[i].weather[0].icon;
         var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
         var imgDiv = $('<img>').attr('src', iconUrl);
         $('#day' + i + '').append('<img src=' + iconUrl + ' />');
+        
+        // Temperature and humidity
         $('#day' + i + '').append('<p>' + 'Temperature: ' + ((Math.floor((response.daily[i].temp.day) - 273.15) * 1.80 + 32)).toFixed(1) + 'F' + '</p>')
         $('#day' + i + '').append('<p>' + 'Humidity: ' + response.daily[i].humidity + '%' + '</p>');
       }
       $('#forecastTab').css('display', 'block');
 
-      // Map Stuff
+      // MAPS DISPLAY //
+      // Convert lat/lon coords to zoom-level tiles
       function lon2tile(lon, zoom) { return (Math.floor((lon + 180) / 360 * Math.pow(2, zoom))); }
       function lat2tile(lat, zoom) { return (Math.floor((1 - Math.log(Math.tan(lat * Math.PI / 180) + 1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom))); }
       var zoom = 3;
       var lonTile = lon2tile(lon, zoom);
       var latTile = lat2tile(lat, zoom);
-      // var temperatureMapUrl = 'https://tile.openweathermap.org/map/temp_new/' + zoom + '/' + lonTile + '/' + latTile + '.png?appid=' + apiKey;
-      // var mapDiv = $('<img>').attr('src', temperatureMapUrl);
-      // $('#temperatureMap').empty();
 
-      // $('#temperatureMap').append(mapDiv);
+      // Temperature, percipitation and wind maps
+      mapMaker('temp_new', 'temperatureMap', 'A map displaying regional temperatures');
+      mapMaker('precipitation_new', 'percipitationMap', 'A map displaying regional percipitation');
+      mapMaker('wind_new', 'windMap', 'A map displaying regional winds');
 
-      // Temperature Map
-      mapMaker('temp_new', 'temperatureMap');
-
-      // Percipitation Map
-      mapMaker('precipitation_new', 'percipitationMap');
-
-            // Percipitation Map
-            mapMaker('wind_new', 'windMap');
-
-      function mapMaker(layer, divID) {
+      // Map rendering
+      function mapMaker(layer, divID, altText) {
         var url = 'https://tile.openweathermap.org/map/' + layer + '/' + zoom + '/' + lonTile + '/' + latTile + '.png?appid=' + apiKey;
-        var mapImg = $('<img>').attr('src', url);
+        var mapImg = $('<img>').attr('src', url).attr('alt', altText);
         $('#' + divID).empty();
         $('.mapDiv').css('display', 'block');
         $('#' + divID).append(mapImg);
       }
     })
   })
-
 }
-
-
